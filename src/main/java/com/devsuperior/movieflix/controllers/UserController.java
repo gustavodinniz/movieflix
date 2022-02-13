7b278f38a729e7b7d791300a819c72753286836a
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-02-12 23:44:18 -0300

    final project

---
+++ src/main/java/com/devsuperior/movieflix/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.devsuperior.movieflix.controllers;
+
+import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = "/profile")
+    public ResponseEntity<UserDTO> getProfile() {
+        UserDTO dto = service.getProfile();
+        return ResponseEntity.ok().body(dto);
+    }
+}
